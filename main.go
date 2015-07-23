1dbae94205d4a3744f4ea0aca7a0d4de1abd9730
Mark Ferree <markferree@gmail.com>
2015-07-22 21:43:45 -0700

    Settings reading shell

--- main.go
+++ main.go
@@ -10,15 +10,15 @@ func main() {
 	if len(os.Args) > 1 {
 
 		commands := map[string]string{
-			"sa": "site-alias",
-			"ls": "pm-list",
+			"sa": "alias",
+			"ls": "list",
 		}
 		for _, command := range commands {
 			currentcommand := os.Args[1]
 			fmt.Printf("Main command %s \n", currentcommand)
 			if currentcommand == "site-alias" {
-				sitealias()
-			} else if command == "" {
+				alias()
+			} else if command == "list" {
 				fmt.Print("Command Not Found")
 			}
 		}
@@ -27,12 +27,29 @@ func main() {
 	}
 }
 
-func sitealias() {
-	aliases, err := ioutil.ReadDir("/home/mark/.drush")
+func alias() {
+	aliases, err := ioutil.ReadDir(os.Getenv("HOME") + "/.drush")
 	for _, fileInfo := range aliases {
 		fmt.Printf("\n File Name : %s \n", fileInfo.Name())
 	}
 	if err != nil {
 		fmt.Printf("ReadDir %s: %v", aliases, err)
+	}
+}
+
+func list() {
+	fmt.Printf("Command not implemented")
+}
+
+func readSettings() {
+	// see if we have alias and read from alias file
+	// current directory and sites/default/files deep
+	currentdir, err := ioutil.ReadDir(".")
+	for _, fileInfo := range currentdir {
+		fmt.Printf("File %s", fileInfo.Name)
+		ioutil.ReadFile("index.php")
+	}
+	if err != nil {
+		fmt.Printf("ReadDir %s: %v", currentdir, err)
 	}
 }
