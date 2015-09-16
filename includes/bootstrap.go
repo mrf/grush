b45bad662e566af93c0688f064bbebd27f45bc61
Mark Ferree <markferree@gmail.com>
2015-09-15 23:05:33 -0700

    Execing PHP from go

--- includes/bootstrap.go
+++ includes/bootstrap.go
@@ -1,10 +1,15 @@
 package includes
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
 // Reference implementation: drush_get_bootstrap_candidates()
 func getBootstrapCandidates() (candidates map[string]Boot) {
 	// @todo: Make static
 	candidates = make(map[string]Boot)
-	candidates["drupal7"] = DrupalBoot7{}
 	candidates["drupal8"] = DrupalBoot8{}
 	return
 }
@@ -18,5 +23,19 @@ func FindBootstrapForRoot(path string) Boot {
 			return c
 		}
 	}
+	return nil
+}
+
+func Boot8(level string) Boot {
+	// todo: include boot level
+	// todo: check if php exists and error out before execution see: exec.LookPath
+
+	cmd := exec.Command("php", "-r print 'we have php';")
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		os.Stderr.WriteString(fmt.Sprintf("==> Error: %s\n", err.Error()))
+	}
+	fmt.Printf("==> Output: %s\n", string(output))
 	return nil
 }
